181d3b8c0030e905092215abbe5781005f615b39
thiunuwan <ravinduthiunuwan@gmail.com>
2024-03-22 10:03:43 +0530

    initial commit

---
+++ src/main/java/com/thiunuwan/helloapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.thiunuwan.helloapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+         return  "hello from web 123 ..";
+    }
+}
